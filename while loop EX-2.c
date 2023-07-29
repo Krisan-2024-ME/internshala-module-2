7465401962ca75226591585ad14e0748f7854341
Krisan Kumar Gupta <138972393+Krisan-2024-ME@users.noreply.github.com>
2023-07-29 17:08:31 +0530

    Add files via upload

---
+++ while loop EX-2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    //WAP to print natural number till 10
+    //Exploring while loop
+    int num = 1;
+    int n;
+    printf("Enter the n:");
+    scanf("%d",&n);
+    while(num <= n ){
+        printf("%d\n",num);
+    num++;
+        
+    }
+    return 0;
+}
