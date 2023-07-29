2c82cceff9b7aec928124ecdb77217cc211127d3
Krisan Kumar Gupta <138972393+Krisan-2024-ME@users.noreply.github.com>
2023-07-29 17:08:13 +0530

    Add files via upload

---
+++ While loop EX-1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    //WAP to print natural number till 10
+    //Exploring while loop
+    int counter = 1;
+    while(counter<=10){
+        printf("%d\n",counter);
+        counter++;
+    }
+    return 0;
+}
